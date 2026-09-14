9eaa242d8bc8a43f6fdce9093366e7690c75d3e1
Darlene-13 <darlenewendie@gmail.com>
2026-09-14 15:56:05 +0300

    Introduction to maps in go

---
+++ day-007/maps.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	age := make(map[string]int)
+}
+
+//Maps in go are just like dictionaries in python and maps in java
+//Make a map of age that matches strings to integers: Names to ages
+//We use make
+//Maps are best used for look-ups
